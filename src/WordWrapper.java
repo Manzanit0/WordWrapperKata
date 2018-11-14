b8fefffe7ee0da3404bf49c8789e0b8d289abdcc
Manzanit0 <unico.javier@gmail.com>
2018-11-14 10:19:10 +0000

    Wraps four letters with a column size of two.

--- src/WordWrapper.java
+++ src/WordWrapper.java
@@ -1,8 +1,9 @@
 public class WordWrapper {
-    public String wrap(String text) {
+    public String wrap(String text, int columnSize) {
         String wrappedText = new String();
-        for(int i = 0; i < text.length(); i++) {
-            wrappedText += text.substring(i, i+1) + "\n";
+
+        for(int i = 0; i < text.length(); i += columnSize) {
+            wrappedText += text.substring(i, i+columnSize > text.length() ? text.length() : i+columnSize) + "\n";
         }
 
         return wrappedText.substring(0, wrappedText.length() - 1);
--- test/WordWrapperTest.java
+++ test/WordWrapperTest.java
@@ -4,15 +4,21 @@ import org.junit.Test;
 public class WordWrapperTest {
     @Test
     public void wrapsTwoLettersWithColumnSizeOne() {
-        WordWrapper wrapper = new WordWrapper();
-        String text = wrapper.wrap("Hi");
-        Assert.assertEquals(text, "H\ni");
+        Assert.assertEquals("H\ni", wrapText("Hi", 1));
     }
 
     @Test
     public void wrapsThreeLettersWithColumnSizeOne() {
+        Assert.assertEquals("T\nw\no", wrapText("Two", 1));
+    }
+
+    @Test
+    public void wrapsFourLettersWithColumnSizeOfTwo() {
+        Assert.assertEquals("Fo\nur", wrapText("Four", 2));
+    }
+
+    private String wrapText(String text, int columnSize) {
         WordWrapper wrapper = new WordWrapper();
-        String text = wrapper.wrap("Two");
-        Assert.assertEquals(text, "T\nw\no");
+        return wrapper.wrap(text, columnSize);
     }
 }
